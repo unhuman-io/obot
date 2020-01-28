587c55ad36e91b371b83160de91c26c2f36c814f
Lee Magnusson <lee@unhuman.io>
2020-01-28 12:19:40 -0800

    working gstreamer

--- jetbot_camera_display.sh
+++ jetbot_camera_display.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gst-launch-1.0 udpsrc port=5000 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! queue ! h264parse ! nvv4l2decoder ! nvdrmvideosink plane_id=1 &
-gst-launch-1.0 udpsrc port=5001 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! queue ! h264parse ! nvv4l2decoder ! nvdrmvideosink offset-x=1920 plane_id=1 &
-gst-launch-1.0 udpsrc port=5002 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! queue ! h264parse ! nvv4l2decoder ! nvdrmvideosink offset-y=1080 plane_id=1 &
+gst-launch-1.0 udpsrc port=5000 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! queue ! h264parse ! nvv4l2decoder ! nvoverlaysink overlay-w=1920 overlay-h=1080 &
+gst-launch-1.0 udpsrc port=5001 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! queue ! h264parse ! nvv4l2decoder ! nvoverlaysink overlay-w=1920 overlay-h=1080 overlay-x=1920 overlay=2 &
+gst-launch-1.0 udpsrc port=5002 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! queue ! h264parse ! nvv4l2decoder ! nvoverlaysink overlay-w=1920 overlay-h=1080 overlay-y=1080 overlay=0 &
