b1ac854cb57f25b300a48ba451d1f2d99d949cbf
Lee Magnusson <lee@unhuman.io>
2020-05-19 12:47:01 -0700

    update install script

--- install-freebot.sh
+++ install-freebot.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-installs=(
+system_installs=(
     https://github.com/unhuman-io/usb_rt_driver/releases/download/0.6.0/usb_rt_driver-0.6.0-Linux.deb 
+)
+
+installs=(
     https://github.com/unhuman-io/realtime-tmp/releases/download/0.4.0/realtime-tmp-0.4.0-Linux.deb
+)
+
+local_installs=(
+     https://github.com/unhuman-io/realtime-tmp/releases/download/0.4.0/artifacts-0.4.0.tgz
 )
 
 sudo apt install -y dkms libudev1 dfu-util wget
 mkdir -p freebot-tmp
 cd freebot-tmp
 
-for install in ${installs[@]}; do
+for install in ${system_installs[@]}; do
     wget $install
     sudo dpkg -i $(basename $install)
 done
+
+if [ $2 == "--local" ]; then
+    for install in ${installs[@]}; do
+        wget $install
+        sudo dpkg -i $(basename $install)
+    done
+else 
+    for install in ${local_installs[@]}; do
+        wget $install
+        tar xzf $(basename $install) -C ../
+    done
+fi
+
 
 cd ..
 rm -r freebot-tmp
