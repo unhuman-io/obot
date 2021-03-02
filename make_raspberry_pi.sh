895bfe33c3ea8f8d352d224576cbe0afac26f2db
Lee Magnusson <lee@unhuman.io>
2021-03-01 16:15:48 -0800

    work on raspberry pi image

---
+++ make_raspberry_pi.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+tmp_dir=$(mktemp -d -t freebot-pi-XXXXXXXX)
+pushd $tmp_dir
+wget https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-01-12/2021-01-11-raspios-buster-armhf-lite.zip
+unzip 2021-01-11-raspios-buster-armhf-lite.zip
+
+#https://wiki.debian.org/RaspberryPi/qemu-user-static
+dd if=/dev/zero bs=1M count=1024 >> 2021-01-11-raspios-buster-armhf-lite.img
+
+loop=$(sudo losetup -f -P --show 2021-01-11-raspios-buster-armhf-lite.img)
+echo $loop
+
+sudo parted -s -a opt $loop "resizepart 2 100%"
+sudo e2fsck -f ${loop}p2
+sudo resize2fs ${loop}p2
+sudo mount ${loop}p2 -o rw /mnt
+
+# install freebot
+cd /mnt
+sudo cp /usr/bin/qemu-arm-static usr/bin
+sudo cp -L /etc/resolv.conf etc/
+sudo systemd-nspawn -D . -M tmpjetson --resolv-conf off --pipe /bin/bash << EOF
+apt update
+apt install -y curl
+curl https://raw.githubusercontent.com/unhuman-io/freebot/main/install-freebot.sh > install-freebot.sh
+chmod +x install-freebot.sh
+arch=armv6l ./install-freebot.sh
+rm install-freebot.sh
+EOF
+sudo rm usr/bin/qemu-arm-static
+sudo rm etc/resolv.conf
+pushd usr/local/src
+sudo sh -c 'curl https://storage.googleapis.com/git-repo-downloads/repo > repo'
+sudo chmod a+rx repo
+sudo git config --global user.name "Root"
+sudo git config --global user.email "root@root.com"
+sudo git config --global color.ui true
+sudo ./repo init -b main -u https://github.com/unhuman-io/freebot
+sudo ./repo sync
+popd
+
+sudo losetup -d $loop
+sudo umount /mnt
+
+popd
