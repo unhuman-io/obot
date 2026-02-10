44df8050d138125cba466454c361907e0ab16f54
leemagnusson <leemagnusson@gmail.com>
2026-02-10 13:08:17 -0800

    improved install pcan driver (#13)

--- install_pcan_driver.sh
+++ install_pcan_driver.sh
@@ -4,6 +4,22 @@ sudo apt install -y libpopt-dev
 wget https://www.peak-system.com/quick/PCAN-Linux-Driver -O pcan-linux-driver.tar.gz
 tar xf pcan-linux-driver.tar.gz
 pushd peak-linux-driver-*
-make -j netdev CC=gcc-12
-sudo make install_with_dkms CC=gcc-12
+make -j netdev #CC=gcc-12
+sudo make install_with_dkms #CC=gcc-12
 sudo sed -i '/fast_fwd/s/^# //g' /etc/modprobe.d/pcan.conf
+echo "options pcan assign=devid" | sudo tee -a /etc/modprobe.d/pcan.conf
+
+cat << EOF | sudo tee -a /etc/systemd/network/20-pcan-canfd.network
+[Match]
+Type=can
+
+[CAN]
+BitRate=1000000
+DataBitRate=8000000
+FDMode=yes
+RestartSec=100ms
+
+EOF
+
+sudo systemctl enable systemd-networkd
+sudo systemctl restart systemd-networkd
